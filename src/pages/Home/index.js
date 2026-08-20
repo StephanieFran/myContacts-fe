99cd4e6d4740b95ffbf5575ffb22cec208ac93e4
Stephanie <stephaniefranca015@gmail.com>
2026-08-20 17:29:07 -0300

    refactor(home | header): changing ContactsList component to Home page and moving SearchBar from Header component to Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,55 @@
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListContainer,
+  Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato" />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Stephanie de França</strong>
+              <small>instagram</small>
+            </div>
+            <span>stephanie@mail.com</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
