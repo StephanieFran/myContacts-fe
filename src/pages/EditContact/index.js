c8c22e0b14dd7d528a464828e71dedba9360b59e
Stephanie <stephaniefranca015@gmail.com>
2026-08-21 17:46:34 -0300

    feat: ContactForm, Input, Select, Button and FormGroup components

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,5 +1,11 @@
 import PageHeader from '../../components/PageHeader';
+import ContactForm from '../../components/ContactForm';
 
 export default function EditContact() {
-  return <PageHeader title="Editar Stephanie França" />;
+  return (
+    <>
+      <PageHeader title="Editar Stephanie França" />
+      <ContactForm buttonLabel="Salvar alterações" />
+    </>
+  );
 }
